04e22bd6c4a3e9865fe992433a276c7929973573
Erik <erik.ondra@yahoo.com>
2022-12-28 11:53:39 +0100

    some styling changes

--- script.js
+++ script.js
@@ -6,15 +6,34 @@ let headerNodeBtn = document.querySelector('.links .black-btn')
 document.addEventListener("scroll", () => {
     lastKnownScrollPosition = window.scrollY
     console.log(lastKnownScrollPosition);
-    if (lastKnownScrollPosition >= 130) {
-        headerNode.style.backgroundColor = "white";
-        headerNodeBtn.style.backgroundColor = "rgb(26, 137, 23)";
+    if (lastKnownScrollPosition >= 370) {
+        console.log('test');
+        headerNode.classList.add('tran-white')
+        headerNode.classList.remove('tran-yellow')
+
+        headerNodeBtn.classList.add('tran-green')
+        headerNodeBtn.classList.remove('tran-black')
     }
-    if (lastKnownScrollPosition <= 128) {
-        headerNode.style.backgroundColor = "rgb(255 192 23)";
-        headerNodeBtn.style.backgroundColor = "black";
+    if (lastKnownScrollPosition < 370) {
+        headerNode.classList.add('tran-yellow')
+        headerNode.classList.remove('tran-white')
+
+        headerNodeBtn.classList.add('tran-black')
+        headerNodeBtn.classList.remove('tran-green')
     }
 })
 
 
 //rgb(26, 137, 23);
+
+
+
+
+
+/* change color in css with classlist.add() */
+/* 
+li
+{
+ color: #000;
+ transition: color 1s ease;
+} */
